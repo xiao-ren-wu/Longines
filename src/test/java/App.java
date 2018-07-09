2dce4efe1b086f46538fe792e42a888a1e4100e9
xiao-ren-wu <18629015421@163.com>
2018-07-09 13:59:30 +0800

    完成项目初始化

---
+++ src/test/java/App.java
@@ -0,0 +1,13 @@
+/**
+ * @author XiaoRenwu
+ * e-mail 18629015421@163.com
+ * github https://github.com/xiao-ren-wu
+ * @version 1
+ * @since 2018/7/8 23:35
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}

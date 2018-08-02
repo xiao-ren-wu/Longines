b06932872331fcb616cb424f62bfb6107af7b4e1
guapi <1617604319@qq.com>
2018-08-02 22:22:02 +0800

    dao层

--- src/main/java/com/longines/dao/SearchMapper.java
+++ src/main/java/com/longines/dao/SearchMapper.java
@@ -2,6 +2,18 @@ package com.longines.dao;
 
 import com.longines.pojo.Search;
 
+import java.util.List;
+
+
 public interface SearchMapper {
-    public void findGoods(String name);
+
+
+    Search getGoodsss(int id);
+
+    //根据商品名称获取商品信息
+    List<Search> getGoods(String name);
+
+    //根据商品主题获取商品信息
+    List<Search> getGoodss(String theme);
+
 }

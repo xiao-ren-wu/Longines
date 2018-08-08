2d929db15140ba87d07bdcce243b9de2340b8051
guapi <1617604319@qq.com>
2018-08-08 22:37:28 +0800

    8月8日代码（Controller层之前完善）

---
+++ src/main/java/com/longines/dao/TbGoodsTheMapper.java
@@ -0,0 +1,28 @@
+package com.longines.dao;
+
+import com.longines.pojo.TbGoodsThe;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author weiyi
+ * @since 2018-08-08 21:41
+ * @version 1.0
+ */
+@Repository("Themedao")
+public interface TbGoodsTheMapper {
+    /**
+     * @return int
+     * @param theDes 主题描述
+     */
+    int selectTheidBytheDes(String theDes);
+
+    /**
+     * @return java.lang.String
+     * @param theDes 主题描述
+     */
+    String selectThepicBythDes(String theDes);
+
+}

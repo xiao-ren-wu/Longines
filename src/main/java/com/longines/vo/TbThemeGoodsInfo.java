7797e84074e5099f9f870ec37262832baa268b89
guapi <1617604319@qq.com>
2018-08-16 23:54:23 +0800

    controller层完成

---
+++ src/main/java/com/longines/vo/TbThemeGoodsInfo.java
@@ -0,0 +1,145 @@
+package com.longines.vo;
+
+public class TbThemeGoodsInfo {
+
+    private Integer gId;
+
+    private String gName;
+
+    private String brand;
+
+    private Long price;
+
+    private Integer theId;
+
+    private String mov;
+
+    private String func;
+
+    private String aPeo;
+
+    private String dial;
+
+    private String strap;
+
+    private String wBut;
+
+    private String gPic;
+
+    private String thePic;
+
+    private String theDes;
+
+    public Integer getgId() {
+        return gId;
+    }
+
+    public void setgId(Integer gId) {
+        this.gId = gId;
+    }
+
+    public String getgName() {
+        return gName;
+    }
+
+    public void setgName(String gName) {
+        this.gName = gName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Integer getTheId() {
+        return theId;
+    }
+
+    public void setTheId(Integer theId) {
+        this.theId = theId;
+    }
+
+    public String getMov() {
+        return mov;
+    }
+
+    public void setMov(String mov) {
+        this.mov = mov;
+    }
+
+    public String getFunc() {
+        return func;
+    }
+
+    public void setFunc(String func) {
+        this.func = func;
+    }
+
+    public String getaPeo() {
+        return aPeo;
+    }
+
+    public void setaPeo(String aPeo) {
+        this.aPeo = aPeo;
+    }
+
+    public String getDial() {
+        return dial;
+    }
+
+    public void setDial(String dial) {
+        this.dial = dial;
+    }
+
+    public String getStrap() {
+        return strap;
+    }
+
+    public void setStrap(String strap) {
+        this.strap = strap;
+    }
+
+    public String getwBut() {
+        return wBut;
+    }
+
+    public void setwBut(String wBut) {
+        this.wBut = wBut;
+    }
+
+    public String getgPic() {
+        return gPic;
+    }
+
+    public void setgPic(String gPic) {
+        this.gPic = gPic;
+    }
+
+    public String getThePic() {
+        return thePic;
+    }
+
+    public void setThePic(String thePic) {
+        this.thePic = thePic;
+    }
+
+    public String getTheDes() {
+        return theDes;
+    }
+
+    public void setTheDes(String theDes) {
+        this.theDes = theDes;
+    }
+
+}
